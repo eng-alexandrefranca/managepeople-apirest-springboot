aa4581cccd30eba011be6cdf4628cb9828335f27
unknown <alexandredasilvafranca@hotmail.com>
2020-12-08 01:51:46 -0300

    commit inicial

---
+++ src/main/java/one/digitalinnovation/managepeople/managepeopleapi/ManagepeopleapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.managepeople.managepeopleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagepeopleapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagepeopleapiApplication.class, args);
+	}
+
+}
